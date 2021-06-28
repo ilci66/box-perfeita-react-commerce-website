80133a304f4e159d887f522a5abd47e8f8b7246f
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-06-28 14:04:18 +0100

    working on navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import { Products } from './Components' 
+import React from 'react';
+import { Products, Navbar } from './Components'; 
 // import everything here
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Products />
     </div>
   )
