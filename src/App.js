7f374d8ec25a70d7499e2ebb583ab67956b8d35d
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-06-29 11:23:44 +0100

    working on cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Products, Navbar } from './Components'; 
 // import everything here
 
 const App = () => {
+  const [ shoppingCart, setShoppingCart ] = React.useState([])
+  let stuff = []
+  const handleAddToCart = (productname) => {
+    stuff.push(productname)
+    setShoppingCart(stuff)
+  }
   return (
-    <div>
-      <Navbar />
-      <Products />
+    <div id="app" >
+      <Navbar shoppingCart={shoppingCart} />
+      <Products onAddToCart={handleAddToCart}/>
     </div>
   )
 }
